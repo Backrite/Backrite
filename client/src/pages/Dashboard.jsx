3f99a4482f5c70a0494b1984386f4feab3bbf1bc
Ayush0095 <aayushsingh7668@gmail.com>
2025-07-31 17:19:00 +0530

    Adding Pages

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+// src/pages/Login.jsx
+function DashBoard() {
+  return (
+    <div>
+      <h2>User DashBoard</h2>
+    </div>
+  );
+}
+
+export default DashBoard;
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+// src/pages/Login.jsx
+function Register() {
+  return (
+    <div>
+      <h2>Register Page</h2>
+    </div>
+  );
+}
+
+export default Register;
