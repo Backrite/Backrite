92c82047b7e3ae0616d7cd20f7c8692ddaba2fed
Ayush0095 <aayushsingh7668@gmail.com>
2025-08-07 09:06:04 +0530

    Connecting Db

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
+const app = express()
 
 // Test Route
 app.get('/', (req, res) => {
