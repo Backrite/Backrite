df9ef7c00cacc7e668066603c3832fdd1b076dd1
Ayush0095 <aayushsingh7668@gmail.com>
2025-08-12 19:28:46 +0530

    Adding problems route endpoints

--- server/app.js
+++ server/app.js
@@ -1,17 +1,23 @@
 import express from 'express';
-import router from './routes/authroutes.js';
 import dotenv from 'dotenv';
+import userRouter from './routes/authroutes.js';
+import problemRouter from './routes/problem.routes.js'; // ✅ new
+
 dotenv.config();
 
 const app = express();
 
-// Routes
-app.use('/user', router);
+// Middleware to parse JSON
+app.use(express.json());
 
+// Routes
+app.use('/user', userRouter);
+//problems endpoints
+app.use('/problems', problemRouter); 
 
 // Test route
 app.get('/', (req, res) => {
-  res.send(' Backrite backend is running!');
+  res.send('Backrite backend is running!');
 });
 
 export default app;
