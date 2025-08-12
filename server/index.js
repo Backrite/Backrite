24bba1ed258a1e182347f15bcf049af7f96c8561
Ayush0095 <aayushsingh7668@gmail.com>
2025-08-12 19:29:34 +0530

    Problem routes with prefix /api/problems

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectDb from "./config/dbConnection.js";
 import authRoutes from "./routes/authroutes.js";
+import problemRoutes from "./routes/problemRoutes.js"
 
 dotenv.config();
 connectDb();
@@ -11,7 +12,11 @@ const port = process.env.PORT  || 5001;
 //middleware
 app.use(express.json());
 
+// Auth routes with prefix /api/auth
 app.use("/api/auth",authRoutes);
+
+// Problem routes with prefix /api/problems
+app.use("/api/problems", problemRoutes);
 
 app.listen(port,()=>{
   console.log(`Server is running on port ${port}`);
