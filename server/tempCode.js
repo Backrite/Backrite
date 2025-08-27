6dd04899dd43b20e3e3a9126762da6a4fef759f5
Ayush0095 <aayushsingh7668@gmail.com>
2025-08-27 18:17:50 +0530

    Making changes for deployment

--- server/tempCode.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); 
-const app = express(); 
-
-app.get('/get', (req, res) => res.send('This is GET route'));
-
-app.post('/post', express.json(), (req, res) => res.json({ 
-    message: 'Received POST', body: req.body })); 
-
-app.listen(3000, () => console.log('Server running'));
